42ba46df337852459e2af6e85386be2e3e771671
David Ebert <ebertek@gmail.com>
2020-09-28 18:14:13 +0200

    Initial commit

---
+++ 2-0928/0928.asm
@@ -0,0 +1,62 @@
+
+Code	Segment
+	assume CS:Code, DS:Data, SS:Stack
+
+Start:
+	mov	ax, Code
+	mov	DS, AX
+
+  ; hello world, karakterenkent:
+  mov AH,2 ; DOS Fn 02H: Send the character in DL to stdout
+  mov DL, 'H'
+  int 21h
+  mov AH,2
+  mov DL, 'e'
+  int 21h
+  mov AH,2
+  mov DL, 'l'
+  int 21h
+  mov AH,2
+  mov DL, 'l'
+  int 21h
+  mov AH,2
+  mov DL, 'o'
+  int 21h
+  mov AH,2
+  mov DL, ' '
+  int 21h
+  mov AH,2
+  mov DL, 'v'
+  int 21h
+  mov AH,2
+  mov DL, 'i'
+  int 21h
+  mov AH,2
+  mov DL, 'l'
+  int 21h
+  mov AH,2
+  mov DL, 'a'
+  int 21h
+  mov AH,2
+  mov DL, 'g'
+  int 21h
+  mov AH,2
+  mov DL, '!'
+  int 21h
+
+Program_Vege:
+	mov ax, 4c00h
+	int 21h
+
+
+
+Code	Ends
+
+Data	Segment
+
+Data	Ends
+
+Stack	Segment
+
+Stack	Ends
+	End	Start
