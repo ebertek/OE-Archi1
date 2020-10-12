549c4fff17eacd3f0238c4fb4704e243c57025d1
David Ebert <ebertek@gmail.com>
2020-10-12 18:03:47 +0200

    Initialize screen

--- 3-1012/1012.asm
+++ 3-1012/1012.asm
@@ -6,6 +6,17 @@ Start:
 	mov	ax, Code
 	mov	DS, AX
 
+; set video mode:
+  mov ax, 03h ; ah: 00h; al: 03h => text, 80x25, 16/8 colors
+  int 10h
+
+; set cursor position:
+  mov ah, 02h
+  mov bh, 0   ; display page number
+  mov dh, 10  ; row
+  mov dl, 0   ; column
+  int 10h
+
 
 
 Program_Vege:
