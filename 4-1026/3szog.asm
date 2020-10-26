0074ed92545fc379b57a19e31be57361f48e1d0c
David Ebert <ebertek@gmail.com>
2020-10-26 18:44:51 +0100

    Draw vertical and horizontal lines of the triangle

--- 4-1026/3szog.asm
+++ 4-1026/3szog.asm
@@ -9,6 +9,41 @@ Start:
 	mov ax, 03 ; clear screen
 	int 10h
 
+Rajz:
+; *
+; **
+; * *
+; *  *
+; *****  <- itt a bal also * 10,10-nel legyen
+
+	mov ah, 2
+	mov bl, 0
+	mov dh, 10 ; sor
+	mov dl, 10 ; oszlop
+	int 10h
+
+; vizszintes resz:
+	mov dl, '*'
+;	mov ah, 2
+	mov cx, 5 ; 5 db * kell
+ciklus1:
+	int 21h
+	loop ciklus1
+
+; fuggoleges resz:
+	mov cl, 6 ; eleg cl-be, nem szukseges cx-be
+ciklus2:
+;	mov ah, 2
+;	mov bl, 0
+	mov dl, 10 ; oszlop fix
+	mov dh, cl
+	int 10h
+	mov dl, '*'
+	int 21h
+	inc cl
+	cmp cl, 11
+	jl ciklus2
+
 Program_vege:
 	mov ax, 4c00h
 	int 21h
